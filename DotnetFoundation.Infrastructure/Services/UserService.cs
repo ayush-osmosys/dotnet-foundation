b6ba9a24811cfd0bf6fad6c384cd74f8d7f02645
harish <harish@osmosys.co>
2023-11-29 16:50:28 +0530

    final running

--- DotnetFoundation.Infrastructure/Services/UserService.cs
+++ DotnetFoundation.Infrastructure/Services/UserService.cs
@@ -2,16 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotnetFoundation.Core.Entities;
+using DotnetFoundation.Core.Interfaces.UserRepo;
 using DotnetFoundation.Core.Services.UserService;
 
 namespace DotnetFoundation.Infrastructure.Services
 {
     public class UserService : IUserService
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         public async Task<Guid> CreateUserAsync(string userName, string email, string password)
         {
             // Implementation of CreateUserAsync method
-            throw NotImplementedException();
+
+            var newUser = new User
+            {
+                Name = userName,
+                Email = email,
+                // Set other properties as needed
+            };
+
+            // Add the user using the UserRepository
+            var user = await _userRepository.AddUser(newUser);
+
+            return Guid.NewGuid(); // Assuming Id is a Guid property in the User class
         }
 
         private Exception NotImplementedException()
